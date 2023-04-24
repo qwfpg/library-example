c2e0f8a1ac877aba1a06f3cd2910a596f9966bf7
Elias Eichelberg <elias@Eliass-Laptop.local>
2023-04-24 14:26:06 +0300

    initial

---
+++ app/Http/Middleware/EmployeeMiddleware.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class EmployeeMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response) $next
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if (!$request->user() || !$request->user()->isEmployee()) {
+            abort(403, 'Unauthorized action.');
+        }
+
+        return $next($request);
+    }
+
+}
