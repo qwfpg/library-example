c97b33c52ab40c1dc5960fafb1bbacccd35b9543
elias <test@test.com>
2023-04-29 23:15:09 +0300

    fix auth users redirect

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -21,7 +21,7 @@ class RedirectIfAuthenticated
 
         foreach ($guards as $guard) {
             if (Auth::guard($guard)->check()) {
-                return redirect(route('admin'));
+                return redirect()->route('books.index');
             }
         }
 
